a0b29f7536c78cc7057f2a54009af86b9d47c765
YasunaMC <wesleyvanneck105@outlook.com>
2018-11-13 07:30:46 +0100

    Update Main.java

--- src/main/java/me/vik1395/ProtectionStones/Main.java
+++ src/main/java/me/vik1395/ProtectionStones/Main.java
@@ -8,7 +8,7 @@ import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.domains.DefaultDomain;
 import com.sk89q.worldguard.protection.flags.*;
-import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
 import org.bukkit.*;
